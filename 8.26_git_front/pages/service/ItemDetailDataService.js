17876fc54d9c19fb5ecb85e2ff0409faa7d6f9ef
yih1612 <yhi1612@naver.com>
2022-08-26 18:26:14 +0900

    team project commit

---
+++ 8.26_git_front/pages/service/ItemDetailDataService.js
@@ -0,0 +1,39 @@
+import http from "~/pages/http-common";
+
+// springboot 연결하기 위한 메소드 정의
+class ItemDetailDataService {
+
+    getItemAll() {
+        return http.get(`/items`);
+    }
+
+    getItemData(idx) {
+        return http.get(`/item/${idx}`);
+    }
+
+    insertCart(data) {
+        return http.post(`/cart`, data);
+    }
+
+    getItemByArea(area) {
+        return http.get(`/item/area/${area}`)
+    }
+
+    // getSearchData(area, itemDate) {
+    //     return http.get(`/search?area=${area}&itemDate=${itemDate}`,);
+    // }
+
+    getSearchData(params) {
+        return http.get(`/search`,{params})
+    }
+
+}
+
+export default new ItemDetailDataService()
+
+
+
+
+
+
+
